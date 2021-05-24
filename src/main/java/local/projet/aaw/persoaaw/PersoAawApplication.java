51faaa858c6511f46676ddbefec613856d0ccb6f
Michael <michael.parmentier@etu.univ-poitiers.fr>
2021-05-24 16:04:43 +0200

    Initial commit - Projet test perso

---
+++ src/main/java/local/projet/aaw/persoaaw/PersoAawApplication.java
@@ -0,0 +1,13 @@
+package local.projet.aaw.persoaaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersoAawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersoAawApplication.class, args);
+    }
+
+}
---
+++ src/test/java/local/projet/aaw/persoaaw/PersoAawApplicationTests.java
@@ -0,0 +1,13 @@
+package local.projet.aaw.persoaaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersoAawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
